e6f02af1bed1f27159026530c73dee1c251645ab
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-11 12:59:49 +1300

    Add new class AttrDefineOperration

--- DBAccess/ConnectedLayer/AttrDefine.cs
+++ DBAccess/ConnectedLayer/AttrDefine.cs
@@ -3,23 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-using System.Data;
 
 namespace DBAccess.ConnectedLayer
 {
     public class AttrDefine
     {
-        private SqlConnection sqlConnection = DBConnection.OpenConnection();
-
-        public void Insert(string tmpId, string attrId, string attrNm, char attrTp, string crter)
-        {
-            string crtDate = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
-            string sql = "INSERT INTO AttrDefine " +  $"(TmpId, AttrId, AttrNm, CrtDate, Crter) Values ('{tmpId}','{attrId}', '{attrNm}', '{crtDate}','{crter}')";
-            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
-            {
-                command.ExecuteNonQuery();
-            }
-        }
+        public string TmpId { get; set; }
+        public string AttrId { get; set; }
+        public string AttrNm { get; set; }
+        public string AttrTp { get; set; }
+        public int LockFlag { get; set; }
+        public Nullable<System.DateTime> CrtDate { get; set; }
+        public string Crter { get; set; }
+        public Nullable<System.DateTime> LstUpdtDate { get; set; }
+        public string LstUpdter { get; set; }
     }
 }

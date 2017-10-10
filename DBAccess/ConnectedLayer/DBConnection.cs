513664b9f612615129962ec7d83905a89617b93a
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-10 21:53:40 +1300

    Add class DBConnection

---
+++ DBAccess/ConnectedLayer/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DBAccess.ConnectedLayer
+{
+    public class DBConnection
+    {
+        public SqlConnection OpenConnection()
+        {
+            
+            SqlConnection DBConnection =  new SqlConnection { ConnectionString = ConfigurationManager.ConnectionStrings["BOMDB"].ToString() };
+            DBConnection.Open();
+            return DBConnection;
+        }
+        public void CloseConnection(SqlConnection sqlConnection)
+        {
+            sqlConnection.Close();
+        }
+    }
+}

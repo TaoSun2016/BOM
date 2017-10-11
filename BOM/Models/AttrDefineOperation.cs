5a3d3132ad5a7466f7c50caf7db7a4f24b16c9a2
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-11 13:10:06 +1300

    remove project dbaccess

---
+++ BOM/Models/AttrDefineOperation.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace DBAccess.ConnectedLayer
+{
+    public class AttrDefineOperation
+    {       
+        public void Insert(string tmpId, string attrId, string attrNm, char attrTp, string crter)
+        {
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+
+            string crtDate = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
+            string sql = "INSERT INTO AttrDefine " + $"(TmpId, AttrId, AttrNm, AttrTp,CrtDate, Crter) Values ('{tmpId}','{attrId}', '{attrNm}', '{attrTp}', '{crtDate}','{crter}')";
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+            DBConnection.CloseConnection(sqlConnection);
+        }
+        public void Delete(string tmpId, string attrId, string attrNm, char attrTp)
+        {
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+            string sql = $"DELETE FROM AttrDefine WHERE TmpId = '{tmpId}' AND AttrId='{attrId}' AND AttrNm='{attrNm}' AND AttrTp='{attrTp}'";
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry! That car is on order!", ex);
+                    throw error;
+                }
+            }
+            DBConnection.CloseConnection(sqlConnection);
+
+        }
+
+        public void Update(string oldTmpId, string oldAttrId, string oldAttrNm, char oldAttrTp, string tmpId, string attrId, string attrNm, char attrTp, string updter)
+        {
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+            string updtDate = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
+
+            string sql = $"UPDATE AttrDefine SET TmpId = '{tmpId}' AND AttrId='{attrId}' AND AttrNm='{attrNm}' AND AttrTp='{attrTp}' AND LstUpdtDate='{updtDate}' AND LstUpdter='{updter}'  WHERE TmpId = '{oldTmpId}' AND AttrId='{oldAttrId}' AND AttrNm='{oldAttrNm}' AND AttrTp='{oldAttrTp}' AND LockFlag='0'";
+
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+            DBConnection.CloseConnection(sqlConnection);
+        }
+
+        public void Lock( string tmpId, string attrId, string attrNm, char attrTp, int lockFlag, string updter)
+        {
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+            string updtDate = DateTime.Now.ToString("yyyyMMdd HH:mm:ss");
+
+            string sql = $"UPDATE AttrDefine SET LockFlag = '{lockFlag}' WHERE TmpId = '{tmpId}' AND AttrId='{attrId}' AND AttrNm='{attrNm}' AND AttrTp='{attrTp}'";
+
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+            DBConnection.CloseConnection(sqlConnection);
+        }
+        public List<AttrDefine> Query(string tmpId, string attrId, string attrNm, string attrTp)
+        {
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+            
+            List<AttrDefine> list = new List<AttrDefine>();
+            
+            string sql = "SELECT * FROM AttrDefine WHERE 0 = 0";
+            if (tmpId != null)
+            {
+                sql += $" AND TmpId = '{tmpId}'";
+            }
+            if (attrId != null)
+            {
+                sql += $" AND AttrId = '{attrId}'";
+            }
+            if (attrNm != null)
+            {
+                sql += $" AND AttrNm = '{attrNm}'";
+            }
+            if (attrTp != null)
+            {
+                sql += $" AND AttrTp = '{attrTp}'";
+            }
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                SqlDataReader dataReader= command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    list.Add(new AttrDefine
+                    {
+                        TmpId = (string)dataReader["TmpId"],
+                        AttrId = (string)dataReader["AttrId"],
+                        AttrNm = (string)dataReader["AttrNm"],
+                        AttrTp = (string)dataReader["AttrTp"],
+                        LockFlag = (int)dataReader["LockFlag"],
+                        CrtDate = (DateTime)dataReader["CrtDate"],
+                        Crter = (string)dataReader["Crter"],
+                        LstUpdtDate = (DateTime)dataReader["LstUpdtDate"],
+                        LstUpdter = (string)dataReader["LstUpdter"],
+
+                    });
+                }
+                dataReader.Close();
+            }
+            DBConnection.CloseConnection(sqlConnection);
+            return list;
+            
+        }
+    }
+}
+
---
+++ BOM/Models/DBConnection.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DBAccess.ConnectedLayer
+{
+    public static class DBConnection
+    {
+        public static SqlConnection OpenConnection()
+        {
+            
+            SqlConnection DBConnection =  new SqlConnection { ConnectionString = ConfigurationManager.ConnectionStrings["BOMDB"].ToString() };
+            DBConnection.Open();
+            return DBConnection;
+        }
+        public static void CloseConnection(SqlConnection sqlConnection)
+        {
+            sqlConnection.Close();
+        }
+    }
+}

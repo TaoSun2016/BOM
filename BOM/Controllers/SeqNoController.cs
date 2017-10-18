b4b2000471bde2cf49654baa62f919c8e81fca72
TaoSun2016 <kevinsun9516@gmail.com>
2017-10-18 16:14:16 +1300

    Add method to get sub seq no

--- BOM/Controllers/SeqNoController.cs
+++ BOM/Controllers/SeqNoController.cs
@@ -13,18 +13,18 @@ namespace BOM.Controllers
     {
         log4net.ILog log = log4net.LogManager.GetLogger("SeqNo");
         [HttpGet]
-        [Route("Get")]
-        public string Get(string tmpId)
+        [Route("GetBase")]
+        public string Get()
         {
             string seqNoString = null;
             try
             {
                 SeqNo seqNo = new SeqNo();
-                seqNoString = seqNo.GetSeqNo(tmpId);
+                seqNoString = seqNo.GetBaseSeqNo();
             }
             catch (Exception e)
             {
-                string logMessage = string.Format($"获取序号出错!TmpId[{tmpId}]\n {e.StackTrace}");
+                string logMessage = string.Format($"获取序号出错!\n {e.StackTrace}");
                 log.Error(logMessage);
                 var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
                 {
@@ -33,8 +33,31 @@ namespace BOM.Controllers
                 };
                 throw new HttpResponseException(responseMessge);
             }
-            return seqNoString;
-            
+            return seqNoString;          
+        }
+
+        [HttpGet]
+        [Route("GetSub")]
+        public long Get(string tmpId)
+        {
+            long newSeqNo = 0;
+            try
+            {
+                SeqNo seqNo = new SeqNo();
+                newSeqNo = seqNo.GetSubSeqNo(tmpId);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"获取序号出错!TmpId=[{tmpId}]\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Get SEQ_NO ERROR"
+                };
+                throw new HttpResponseException(responseMessge);
+            }
+            return newSeqNo;
         }
 
     }
--- BOM/Models/SeqNo.cs
+++ BOM/Models/SeqNo.cs
@@ -6,49 +6,121 @@ namespace BOM.Models
     public partial class SeqNo
     {
         public string Ind_Key { get; set; }
-        public long Next_To { get; set; }
+        public long Next_No { get; set; }
 
-        public string GetSeqNo(string tmpId)
+        log4net.ILog log = log4net.LogManager.GetLogger("SeqNo");
+        public string GetBaseSeqNo()
         {
             int result = 0;
             string seqNo = null;
-            log4net.ILog log = log4net.LogManager.GetLogger("SeqNo");
+
+            SqlConnection sqlConnection = DBConnection.OpenConnection();
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            string sql = @"SELECT * FROM SEQ_NO WHERE Ind_Key = '0'";
+
+
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+
+                command.Transaction = sqlTransaction;
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                if (dataReader.HasRows)
+                {
+                    dataReader.Read();
+
+                    this.Ind_Key = dataReader["IND_KEY"].ToString();
+                    this.Next_No = (long)dataReader["NEXT_NO"];
+
+                    dataReader.Close();
+
+                    seqNo = Convert.ToString(this.Next_No, 8) + "9";
+                    this.Next_No += 1;
+                    sql = $"UPDATE SEQ_NO SET NEXT_NO = {this.Next_No} WHERE IND_KEY = '0'";
+                }
+                else
+                {
+                    dataReader.Close();
+                    DBConnection.CloseConnection(sqlConnection);
+                    throw new Exception("There is no base parameter!");
+                }
+
+                try
+                {
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                    if (result == 0)
+                    {
+                        sqlTransaction.Rollback();
+                        DBConnection.CloseConnection(sqlConnection);
+                        throw new Exception("update table seq_no error!");
+                    }
+
+                    sql = $"INSERT INTO SEQ_NO VALUES ('{seqNo}',0)";
+                    command.CommandText = sql;
+                    result = command.ExecuteNonQuery();
+                    if (result == 0)
+                    {
+                        sqlTransaction.Rollback();
+                        DBConnection.CloseConnection(sqlConnection);
+                        throw new Exception("insert table seq_no error!");
+                    }
+                }
+                catch
+                {
+                    sqlTransaction.Rollback();
+                    DBConnection.CloseConnection(sqlConnection);
+                    throw;
+                }
+                sqlTransaction.Commit();
+            }
+
+            DBConnection.CloseConnection(sqlConnection);
+            return seqNo;
+        }
+
+        public long GetSubSeqNo(string tmpId)
+        {
+            int result = 0;
+            long seqNo = 0;
+
             SqlConnection sqlConnection = DBConnection.OpenConnection();
             string sql = $"SELECT * FROM SEQ_NO WHERE Ind_Key = '{tmpId}'";
-
 
             using (SqlCommand command = new SqlCommand(sql, sqlConnection))
             {
                 SqlDataReader dataReader = command.ExecuteReader();
 
-
                 if (dataReader.HasRows)
                 {
                     dataReader.Read();
-                    this.Ind_Key = dataReader["IND_KEY"].ToString();
 
-                    this.Next_To = (long)dataReader["NEXT_NO"];
+                    this.Ind_Key = dataReader["IND_KEY"].ToString();
+                    this.Next_No = (long)dataReader["NEXT_NO"];
+
                     dataReader.Close();
-                    this.Next_To += 1;
-                    sql = $"UPDATE SEQ_NO SET NEXT_NO = {this.Next_To} WHERE IND_KEY = '{this.Ind_Key}'";
+
+                    seqNo = this.Next_No;
+                    this.Next_No += 1;
+                    sql = $"UPDATE SEQ_NO SET NEXT_NO = {this.Next_No} WHERE IND_KEY = '{tmpId}'";
                 }
                 else
                 {
-                    this.Ind_Key = tmpId;
-                    this.Next_To = 1;
+                    log.Error(string.Format($"找不到物料参数记录,TmpID=[{tmpId}]"));
                     dataReader.Close();
-                    this.Next_To += 1;
-                    sql = $"INSERT INTO SEQ_NO VALUES ('{this.Ind_Key}',{this.Next_To}";
+                    DBConnection.CloseConnection(sqlConnection);
+                    throw new Exception(string.Format($"找不到物料参数记录,TmpID=[{tmpId}]"));
                 }
-                seqNo = Convert.ToString(this.Next_To,8)+"9";
-                command.CommandText = sql;
 
+                command.CommandText = sql;
                 result = command.ExecuteNonQuery();
                 if (result == 0)
                 {
+                    log.Error($"更新序号表出错,TmpID=[{tmpId}]");
                     DBConnection.CloseConnection(sqlConnection);
-                    throw new Exception("update/insert table seq_no error!");
+                    throw new Exception($"更新序号表出错,TmpID=[{tmpId}]");
                 }
+
             }
 
             DBConnection.CloseConnection(sqlConnection);

b844cc71def2971fed10d619e04bb32052e0a4f7
TaoSun2016 <kevinsun9516@gmail.com>
2017-11-07 14:58:07 +1300

    Add new controller BOMController

---
+++ BOM/Controllers/BOMController.cs
@@ -0,0 +1,126 @@
+﻿using BOM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BOM.Controllers
+{
+    [RoutePrefix("BOM")]
+    public class BOMController : ApiController
+    {
+        log4net.ILog log = log4net.LogManager.GetLogger("Templet");
+        [HttpPost]
+        [Route("CreateRoot")]
+        public void CreateRoot(string templetName, string creater)
+        {
+            Templet templet = new Templet();
+            try
+            {
+                templet.CreateTemplet(templetName, creater);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"Create root templet error!\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Create root templet ERROR"
+                };
+                throw new HttpResponseException(responseMessge);
+            }      
+        }
+
+        [HttpPost]
+        [Route("CreateChild")]
+        public void CreateChildWithoutTemplet(long parentTempletId,string templetName, string creater)
+        {
+            Templet templet = new Templet();
+            try
+            {
+                templet.CreateTemplet(parentTempletId,templetName, creater);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"Create child templet error!\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Create child templet ERROR"
+                };
+                throw new HttpResponseException(responseMessge);
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateWithTemplet")]
+        public void CreateCopiedTemplet(long parentTempletId, long referenceTempletId, string creater)
+        {
+            Templet templet = new Templet();
+            try
+            {
+                templet.CreateCopiedTemplet(parentTempletId, referenceTempletId, creater);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"Create child templet error!\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Create child templet ERROR"
+                };
+                throw new HttpResponseException(responseMessge);
+            }
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public void DeleteTemplet(long parentTempletId, long TempletId, int rlSeqNo)
+        {
+            Templet templet = new Templet();
+            try
+            {
+                templet.DeleteTemplet( parentTempletId, TempletId, rlSeqNo);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"Delete templet error!\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Delete templet ERROR"
+                };
+                throw new HttpResponseException(responseMessge);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Lock")]
+        public void LockTemplet(long templetId)
+        {
+            Templet templet = new Templet();
+            try
+            {
+                templet.LockTemplet(templetId);
+            }
+            catch (Exception e)
+            {
+                string logMessage = string.Format($"Lock templet error!\n {e.StackTrace}");
+                log.Error(logMessage);
+                var responseMessge = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+                {
+                    Content = new StringContent(logMessage),
+                    ReasonPhrase = "Lock templet error"
+                };
+                throw new HttpResponseException(responseMessge);
+            }
+        }
+    }
+}
